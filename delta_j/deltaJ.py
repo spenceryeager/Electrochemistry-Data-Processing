e891cb4d74bfc63cce67a738cba63ca54ee73d94
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-22 22:27:58 -0700

    implemented selection function, need to pick data

--- delta_j/deltaJ.py
+++ delta_j/deltaJ.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.widgets as mwidgets
 import numpy as np
 import pandas as pd
 
@@ -6,7 +7,18 @@ def main():
     #specify path for now, implement file grab later
     workingfile = '/home/spenceryeager/Documents/calculations/chopper_data/Hour 23 1000mA.txt'
     data = pd.read_csv(workingfile, sep=',', skiprows=rowskip(workingfile))
-    print(data)
+    fig, ax = plt.subplots()
+    ax.plot(data['Potential/V'], data[' Current/A'], color='blue')
+    rectprops = dict(facecolor='blue', alpha=0.4)
+    span = mwidgets.SpanSelector(ax, onselect, 'horizontal', rectprops=rectprops)
+    plt.show()
+
+
+def onselect(vmin, vmax):
+    global max_compval
+    global min_compval
+    max_compval = vmax
+    min_compval = vmin
 
 
 def rowskip(working_file):
