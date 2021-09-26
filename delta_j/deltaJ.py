2f4c5c882ed875f3dacc4dd57c0523dbab78bf5b
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-25 19:27:15 -0700

    autopep8

--- delta_j/deltaJ.py
+++ delta_j/deltaJ.py
@@ -8,34 +8,40 @@ import tkinter.filedialog as fd
 root = Tk()
 root.withdraw()
 
+
 def main():
-    #specify path for now, implement file grab later
+    # specify path for now, implement file grab later
     # workingfile = '/home/spenceryeager/Documents/calculations/chopper_data/Hour 23 1000mA.txt'
     workingfile = fd.askopenfilename()
-    global directory 
+    global directory
     # directory = '/home/spenceryeager/Documents/calculations/chopper_data/'
     directory = os.path.dirname(workingfile)
     print(directory)
-    global data # data must be global to be called in onselect function
-    data = pd.read_csv(workingfile, sep=',', skiprows=rowskip(workingfile)) 
-    columns = ['Potential (V)', 'Dark Current (A)', 'On Current (A)', 'Delta Current (A)']
-    global output_data # generated data will be appended to this
-    output_data = pd.DataFrame(columns = columns)
-    
-    #Plotting
+    global data  # data must be global to be called in onselect function
+    data = pd.read_csv(workingfile, sep=',', skiprows=rowskip(workingfile))
+    columns = ['Potential (V)', 'Dark Current (A)',
+               'On Current (A)', 'Delta Current (A)']
+    global output_data  # generated data will be appended to this
+    output_data = pd.DataFrame(columns=columns)
+
+    # Plotting
     fig, ax = plt.subplots()
     ax.plot(data['Potential/V'], data[' Current/A'], color='blue')
     ax.set_xlabel('Potential (V)')
     ax.set_ylabel('Current (A)')
-    ax.set_title('Select the potential range that contains the on/off current\nClose the graph when finished selecting points')
+    ax.set_title(
+        'Select the potential range that contains the on/off current\nClose the graph when finished selecting points')
     rectprops = dict(facecolor='blue', alpha=0.4)
-    span = mwidgets.SpanSelector(ax, onselect, 'horizontal', rectprops=rectprops)
+    span = mwidgets.SpanSelector(
+        ax, onselect, 'horizontal', rectprops=rectprops)
     plt.show()
-    
+
     # Double checking selected points with graph
     plt.plot(data['Potential/V'], data[' Current/A'], color='blue')
-    plt.scatter(output_data['Potential (V)'], output_data['Dark Current (A)'], label='Off Current', marker='o', color='red')
-    plt.scatter(output_data['Potential (V)'], output_data['On Current (A)'], label='On Current', marker='o', color='darkorange')
+    plt.scatter(output_data['Potential (V)'], output_data['Dark Current (A)'],
+                label='Off Current', marker='o', color='red')
+    plt.scatter(output_data['Potential (V)'], output_data['On Current (A)'],
+                label='On Current', marker='o', color='darkorange')
     plt.title('Double check points and make sure they look reasonable')
     plt.xlabel('Potential (V)')
     plt.ylabel('Current (A)')
@@ -43,6 +49,7 @@ def main():
     plt.show()
 
     filemake(output_data, directory)
+
 
 def onselect(vmin, vmax):
     indmin, indmax = np.searchsorted(data['Potential/V'], (vmin, vmax))
@@ -64,12 +71,11 @@ def selection(indmin, indmax):
         medianV = np.median(potential_array[inddark:indon])
         append = [medianV, dark_current, on_current, delta_current]
         output_data.loc[len(output_data)] = append
-        
+
     else:
         medianV = np.median(potential_array[indon:inddark])
         append = [medianV, dark_current, on_current, delta_current]
         output_data.loc[len(output_data)] = append
-
 
 
 def filemake(data, filepath):
